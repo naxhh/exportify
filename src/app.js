c7d1949583408182984ab3a4abc542ecb81d687b
Ignacio Tolstoy <ignacio.tolstoy@scmspain.com>
2017-07-07 21:38:01 +0200

    feat(components): react router v4

--- src/app.js
+++ src/app.js
@@ -1,12 +1,15 @@
 import ReactDOM from 'react-dom'
 import React from 'react'
 import injectTapEventPlugin from 'react-tap-event-plugin'
+import { BrowserRouter } from 'react-router-dom'
 import App from './components/App'
 
 //Needed for material-ui
 injectTapEventPlugin()
 
 ReactDOM.render(
-	<App />,
+	<BrowserRouter>
+		<App />
+	</BrowserRouter>,
 	document.getElementById('root')
 )
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,29 @@
 import React from 'react'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
-import RaisedButton from 'material-ui/RaisedButton';
+import RaisedButton from 'material-ui/RaisedButton'
+import { Route, Link } from 'react-router-dom'
+
+
+const Home = () =>
+	<div>
+		<nav>
+			<Link to='/test'>Test</Link>
+		</nav>
+		<div>
+			<h1>hi</h1>
+			<RaisedButton label="Default" />
+		</div>
+	</div>
+
+const Test = () =>
+	<h1>Hi and all</h1>
 
 
 const App = () =>
 	<MuiThemeProvider>
 		<div>
-			<h1>hi</h1>
-			<RaisedButton label="Default" />
+			<Route exact path='/' component={Home} />
+			<Route path='/test' component={Test} />
 		</div>
 	</MuiThemeProvider>
 
