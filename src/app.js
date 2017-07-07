6191182a771be60e99b8212108762b069a7b0fa0
Ignacio Tolstoy <ignacio.tolstoy@scmspain.com>
2017-07-07 21:05:49 +0200

    feat(components): material UI

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
 import ReactDOM from 'react-dom'
 import React from 'react'
+import injectTapEventPlugin from 'react-tap-event-plugin'
 import App from './components/App'
+
+//Needed for material-ui
+injectTapEventPlugin()
 
 ReactDOM.render(
 	<App />,
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import RaisedButton from 'material-ui/RaisedButton';
 
-const App = () => {
-	return <h1>hi</h1>
-}
+
+const App = () =>
+	<MuiThemeProvider>
+		<div>
+			<h1>hi</h1>
+			<RaisedButton label="Default" />
+		</div>
+	</MuiThemeProvider>
 
 export default App
