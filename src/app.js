0d231c1c02206e689b0aefd58430c75b467b3fe1
Ignacio Tolstoy <ignacio.tolstoy@scmspain.com>
2017-07-07 20:48:04 +0200

    feat(components): move to App component

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 import ReactDOM from 'react-dom'
 import React from 'react'
+import App from './components/App'
 
 ReactDOM.render(
-	<div>hello</div>,
+	<App />,
 	document.getElementById('root')
 )
